9f3e16271591b7867362b75a9209e6a7783fc52a
Zach Hafen <zachary.h.hafen@gmail.com>
2019-12-03 16:34:13 -0600

    Added ray filename function.

--- stained_glass/utils/file_management.py
+++ stained_glass/utils/file_management.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 '''Simple functions and variables for easily accessing common files and choices
 of parameters.
-
-@author: Zach Hafen
-@contact: zachary.h.hafen@gmail.com
-@status: Development
 '''
 
 import os
@@ -41,46 +37,92 @@ class FileManager( object ):
             }
             physics = name_mapping[sim_name[4:]]
 
+        if resolution is None:
+            resolution = sg_config.DEFAULT_SIM_RESOLUTIONS[sim_name]
+
         return os.path.join(
             physics,
             '{}_res{}'.format(
                 sim_name[:4],
-                sg_config.DEFAULT_SIM_RESOLUTIONS[sim_name],
+                resolution,
             )
         )
 
     ########################################################################
 
-    def get_sim_dir( self, sim_name ):
+    def get_sim_dir( self, sim_name, **kwargs ):
 
         return os.path.join(
             self.system_parameters['simulation_data_dir'],
-            self.get_sim_subdir( sim_name ),
+            self.get_sim_subdir( sim_name, **kwargs ),
             'output',
         )
 
     ########################################################################
 
-    def get_metafile_dir( self, sim_name ):
+    def get_yt_filepath( self, sim_name, snum, **kwargs ):
+
+        formatted_snum = '{:03d}'.format( snum )
+
+        # Assume there are subdirectories
+        filepath = os.path.join(
+            self.get_sim_dir( sim_name, **kwargs ),
+            'snapdir_{}'.format( formatted_snum ),
+            'snapshot_{}.0.hdf5'.format( formatted_snum )
+        )
+
+        # If there are not subdirectories change the filepath
+        if not os.path.isfile( filepath ):
+            filepath = os.path.join(
+                self.get_sim_dir( sim_name, **kwargs ),
+                'snapshot_{}.hdf5'.format( formatted_snum )
+            )
+
+        return filepath
+
+    ########################################################################
+
+    def get_ray_filepath( self, sim_name, snum, **kwargs ):
+
+        data_dir = self.get_stained_glass_dir( sim_name, **kwargs )
+
+        # Choose the next empty ray name
+        empty_filename = 'ray_{}.h5'
+        i = 0
+        while True:
+            
+            filename = empty_filename.format( i )
+            filepath = os.path.join( data_dir, filename )
+
+            if not os.path.isfile( filepath ):
+                break
+            
+            i += 1
+
+        return filepath
+
+    ########################################################################
+
+    def get_metafile_dir( self, sim_name, **kwargs ):
 
         return os.path.join(
             self.system_parameters['simulation_data_dir'],
-            self.get_sim_subdir( sim_name ),
+            self.get_sim_subdir( sim_name, **kwargs ),
         )
 
     ########################################################################
 
-    def get_halo_dir( self, sim_name ):
+    def get_halo_dir( self, sim_name, **kwargs ):
 
         return os.path.join(
             self.system_parameters['halo_data_dir'],
-            self.get_sim_subdir( sim_name ),
+            self.get_sim_subdir( sim_name, **kwargs ),
             'halo',
         )
 
     ########################################################################
 
-    def get_stained_glass_dir( self, sim_name, subdir='data', ):
+    def get_stained_glass_dir( self, sim_name, subdir='data', **kwargs ):
         
         return os.path.join(
             self.system_parameters['stained_glass_data_dir'],
