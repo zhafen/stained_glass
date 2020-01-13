fb3938f09c3d216c38090a7023df8a9adbfb5d0b
Zach Hafen <zachary.h.hafen@gmail.com>
2020-01-13 11:15:09 -0600

    Added alternate correlation function estimators.

--- stained_glass/stats.py
+++ stained_glass/stats.py
@@ -16,10 +16,12 @@ def two_point_cf(
     mins,
     maxs,
     n_bins = 16,
+    estimator = 'ls',
 ):
-    '''Two-point radial autocorrelation function, evaluated using the
-    Landy&Szalay1993 metric. A value of 0 means no correlation beyond that
-    expected due to randomness.
+    '''Two-point radial autocorrelation function. A value of 0 means no
+    correlation beyond that expected due to randomness. 1 + xi, where
+    xi is the two-point correlation function is the probability of finding a
+    point at that distance.
 
     Args:
         coords (array-like, (n_samples, n_dimensions):
@@ -30,6 +32,9 @@ def two_point_cf(
 
         n_bins (int):
             Number of bins over which to evaluate the metric.
+
+        estimator (str):
+            CF estimator to use. Defaults to the Landy&Szalay1993 metric.
 
     Returns:
         A tuple containing...
@@ -61,13 +66,26 @@ def two_point_cf(
         randoms,
     )
 
-    # Choose only unique distances
+    # Choose only unique pairs
     dd_dists = np.tril( dd_dists )
     rr_dists = np.tril( rr_dists )
 
     # Create radial bins
     r_max = np.sqrt( ( ( maxs - mins )**2. ).sum() )
     edges = np.linspace( 0., r_max, n_bins+1 )
+
+    # Estimators
+    def ls( n_dd, n_dr, n_rr ):
+        return ( n_dd - 2. * n_dr + n_rr ) / n_rr
+    def simple( n_dd, n_dr, n_rr ):
+        return n_dd / n_rr - 1.
+    def dp( n_dd, n_dr, n_rr ):
+        return n_dd / n_dr - 1.
+    estimators = {
+        'ls': ls,
+        'simple': simple,
+        'dp': dp,
+    }
 
     # Function for counting number of pairs
     def count( dists, inner, outer ):
@@ -94,8 +112,12 @@ def two_point_cf(
         n_dr /= n_samples**2.
         n_rr /= n_samples * ( n_samples - 1 ) / 2.
 
-        ls_estimator = ( n_dd - 2. * n_dr + n_rr ) / n_rr
-        result.append( ls_estimator )
+        bin_result = estimators[estimator]( n_dd, n_dr, n_rr )
+        result.append( bin_result )
+
+        # if ls_estimator < -1:
+        #     #DEBUG
+        #     import pdb; pdb.set_trace()
 
     result = np.array( result )
 
