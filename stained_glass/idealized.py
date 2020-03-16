848123ce2161bfc505f51a63f03563c763efc4e6
Zach Hafen <zachary.h.hafen@gmail.com>
2020-03-16 16:53:12 -0500

    Added functionality for adding idealized projected ellipses.

--- stained_glass/idealized.py
+++ stained_glass/idealized.py
@@ -5,6 +5,9 @@
 import copy
 import numpy as np
 
+import shapely.geometry as geometry
+import shapely.affinity as affinity
+
 from augment.augment import store_parameters
 
 ########################################################################
@@ -15,8 +18,24 @@ class IdealizedProjection( object ):
     def __init__( self, sidelength=20., c=(0,0), ):
         '''A class for creating idealized projections of physical
         distributions and mock observing them.
+
+        Args:
+            sidelength (float):
+                Sidelength of the square containing the projection.
+
+            c (tuple of floats, (2,)):
+                Center of the square containing the projection
         '''
-        pass
+
+        # For storing structures
+        self.structs = []
+        self.struct_values = []
+
+        # Parameters based off of input
+        self.x_min = self.c[0] - self.sidelength/2
+        self.x_max = self.c[0] + self.sidelength/2
+        self.y_min = self.c[1] - self.sidelength/2
+        self.y_max = self.c[1] + self.sidelength/2
 
     ########################################################################
     # Mock Observations
@@ -30,30 +49,117 @@ class IdealizedProjection( object ):
             seed (int): Random seed to use for generating.
 
         Modifes:
-            self.sl_xs (np.ndarray, (n,) ): X-positions of sightlines.
-            self.sl_ys (np.ndarray, (n,) ): Y-positions of sightlines.
+            self.n (int):
+                Number of sightlines generated
+
+            self.sl_xs (np.ndarray, (n,) ):
+                X-positions of sightlines.
+
+            self.sl_ys (np.ndarray, (n,) ):
+                Y-positions of sightlines.
+
+            self.sls (MultiPoint, (n,) ):
+                Collection of shapely Point objects representing sightlines.
         '''
         
         if seed is not None:
             np.random.seed( seed )
 
-        self.sl_xs = np.random.uniform(
-            self.c[0] - self.sidelength/2.,
-            self.c[0] + self.sidelength/2.,
-            n
-        )
-        self.sl_ys = np.random.uniform(
-            self.c[1] - self.sidelength/2.,
-            self.c[1] + self.sidelength/2.,
-            n
-        )
+        # Store and generate
+        self.n = n
+        self.sl_xs = np.random.uniform( self.x_min, self.x_max, n )
+        self.sl_ys = np.random.uniform( self.y_min, self.y_max, n )
+        self.sls = geometry.MultiPoint( list( zip( self.sl_xs, self.sl_ys ) ) )
+
+    ########################################################################
+
+    def evaluate_sightlines( self, ):
+
+        # Loop over all structures
+        vs = np.zeros( self.n )
+        for i, s in enumerate( self.structs ):
+
+            # Check if inside and if so add
+            inside_s = s.contains( self.sls )
+            vs[inside_s] += self.struct_values[i]
+
+        return vs
 
     ########################################################################
     # Idealized Structure
     ########################################################################
 
     def add_background( self, value=1. ):
-        '''Add a background distribution
+        '''Add a background distribution.
+
+        Args:
+            value (float):
+                On-sky value associated with the structure.
+
+        Modifies:
+            self.structs (list of shapely objects):
+                Adds background shape to the list of structures.
+
+            self.struct_values (list of floats):
+                Adds associated value.
         '''
 
-        pass
+        # Create
+        s = geometry.Polygon( [
+            ( self.x_min, self.y_min, ),
+            ( self.x_min, self.y_max, ),
+            ( self.x_max, self.y_max, ),
+            ( self.x_max, self.y_min, ),
+        ] )
+
+        # Store
+        self.structs.append( s )
+        self.struct_values.append( value )
+
+    ########################################################################
+
+    def add_ellipse( self, c, a, b=None, rotation=None, value=1. ):
+        '''Add an ellipse or circle.
+
+        Args:
+            c (tuple of floats, (2,)):
+                Center of the ellipse.
+
+            a (float):
+                Radius of the circle (if b is None) or axis of the ellipse.
+
+            b (float or None):
+                If given second axis of the ellipse.
+
+            rotation (float):
+                Rotation of the ellipse from the x-axis in degrees.
+
+            value (float):
+                On-sky value associated with the structure.
+
+        Modifies:
+            self.structs (list of shapely objects):
+                Adds ellipse structure to the list of structures.
+
+            self.struct_values (list of floats):
+                Adds associated value.
+        '''
+
+        # Ellipse or circle
+        if b is None:
+            b = a
+
+        # Create a circle
+        circ = geometry.Point( c ).buffer(1.) 
+
+        # Shape it into an ellipse
+        ellipse = affinity.scale( circ, a, b )
+
+        # Rotate
+        if rotation is not None:
+            ellipse = affinity.rotate( ellipse, rotation )
+
+        # Store
+        self.structs.append( ellipse )
+        self.struct_values.append( value )
+
--- stained_glass/tests/test_idealized.py
+++ stained_glass/tests/test_idealized.py
@@ -19,7 +19,7 @@ import stained_glass.idealized as idealized
 ########################################################################
 ########################################################################
 
-class TestIdealizedProjection( unittest.TestCase ):
+class TestMockObserve( unittest.TestCase ):
 
     def test_generate_sightlines( self ):
 
@@ -36,3 +36,109 @@ class TestIdealizedProjection( unittest.TestCase ):
         assert ip.sl_ys.min() > -10.
         assert ip.sl_xs.max() < 10.
         assert ip.sl_ys.max() < 10.
+
+    ########################################################################
+
+    def test_evaluate_sightlines( self ):
+
+        # Setup
+        ip = idealized.IdealizedProjection()
+        ip.generate_sightlines( 1000, seed=1234 )
+        value = 1.
+        ip.add_background( value )
+
+        # Evaluate
+        vs = ip.evaluate_sightlines()
+
+        # Test
+        npt.assert_allclose( vs, np.full( ( ip.n, ), value, ) )
+
+    ########################################################################
+
+    def test_evaluate_sightlines_two_structs( self ):
+
+        # Setup
+        ip = idealized.IdealizedProjection()
+        ip.generate_sightlines( 1000, seed=1234 )
+        value = 1.
+        ip.add_background( value )
+        ip.add_ellipse( value=value )
+
+        # Evaluate
+        vs = ip.evaluate_sightlines()
+        is_value = np.isclose( vs, value )
+        is_twice_value = np.isclose( vs, 2.*value )
+        # The number of points with that value should scale as the area of
+        # the ellipse
+        npt.assert_allclose(
+            is_twice_value.sum() / ip.n,
+            ip.structs[1].area / ip.structs[0].area,
+            rtol = 1e-2
+        )
+
+########################################################################
+
+class TestAddStructures( unittest.TestCase ):
+
+    def setUp( self ):
+
+        self.ip = idealized.IdealizedProjection()
+
+        # Generate sightlines
+        n = 1000
+        self.ip.generate_sightlines( n, seed=1234 )
+
+    ########################################################################
+
+    def test_add_background( self ):
+
+        # Function itself
+        self.ip.add_background()
+
+        # Check output
+        npt.assert_allclose(
+            self.ip.structs[0].bounds,
+            ( -10., -10., 10., 10., ),
+        )
+
+    ########################################################################
+
+    def test_add_circle( self ):
+
+        # Function itself
+        center = ( 1., 2. )
+        radius = 2.
+        self.ip.add_ellipse(
+            center,
+            radius,
+        )
+
+        # Check output
+        npt.assert_allclose(
+            self.ip.structs[0].area,
+            np.pi * radius**2.,
+            rtol = 1e-2,
+        )
+
+    ########################################################################
+
+    def test_add_ellipse( self ):
+
+        # Function itself
+        center = ( 1., 2. )
+        a = 2.
+        b = 3.
+        rotation = 45.
+        self.ip.add_ellipse(
+            center,
+            a,
+            b,
+            rotation = rotation,
+        )
+
+        # Check output
+        npt.assert_allclose(
+            self.ip.structs[0].area,
+            np.pi * a * b,
+            rtol = 1e-2,
+        )
